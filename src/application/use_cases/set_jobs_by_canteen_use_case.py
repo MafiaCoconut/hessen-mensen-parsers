4f067328d7e8135c1656cf15331e12830e636728
MafiaCoconut <alex.kach.2000@gmail.com>
2024-08-11 02:47:27 +0200

    добавил отлавливатель добавления уже существующих работ

--- src/application/use_cases/set_jobs_by_canteen_use_case.py
+++ src/application/use_cases/set_jobs_by_canteen_use_case.py
@@ -11,18 +11,22 @@ class SetJobsByCanteenUseCase:
 
     async def execute(self, canteen: Canteen, func):
         if canteen.status == "active":
-            times: dict = canteen.times
-            times_keys = list(times.keys())
-            for i in range(len(times_keys)):
-                await self.scheduler_interface.add_job(
-                    Job(
-                        func=func,
-                        trigger='cron',
-                        id=f"parser_{canteen.canteen_id}_{times_keys[i]}",
-                        hour=times.get(times_keys[i]).get('hour'),
-                        minute=times.get(times_keys[i]).get('minute'),
-                        day_of_week='mon-fri',
-                        args=[canteen.canteen_id]
+            try:
+                times: dict = canteen.times
+                times_keys = list(times.keys())
+                for i in range(len(times_keys)):
+                    await self.scheduler_interface.add_job(
+                        Job(
+                            func=func,
+                            trigger='cron',
+                            id=f"parser_{canteen.canteen_id}_{times_keys[i]}",
+                            hour=times.get(times_keys[i]).get('hour'),
+                            minute=times.get(times_keys[i]).get('minute'),
+                            day_of_week='mon-fri',
+                            args=[canteen.canteen_id]
+                        )
                     )
-                )
+            except Exception as e:
+                print(f"Error setting jobs for canteen {canteen.canteen_id}: {e}")
+
 
